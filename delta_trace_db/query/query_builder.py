277af47200a7711b1096ee31ae98f52456dba3b9
MasahideMori-SimpleAppli <59500584+MasahideMori-SimpleAppli@users.noreply.github.com>
2025-11-13 21:17:01 +0900

    pre v0.0.37. Update documentation.

--- delta_trace_db/query/query_builder.py
+++ delta_trace_db/query/query_builder.py
@@ -337,8 +337,8 @@ class QueryBuilder:
 
             - Optional. If omitted, results will be returned in the order they were added to the database.
         offset: Optional[int]
-            An offset for paging support in the front end.
-            If specified, data from the offset onwards will be retrieved.
+            An offset for front-end paging support.
+            If specified, data after the specified offset will be retrieved.
         start_after: Optional[Dict[str, Any]]
             If you pass in a serialized version of a search result
             object, the search will return results from objects after that object,
@@ -435,8 +435,8 @@ class QueryBuilder:
 
             - Optional. If omitted, results will be returned in the order they were added to the database.
         offset: Optional[int]
-            An offset for paging support in the front end.
-            If specified, data from the offset onwards will be retrieved.
+            An offset for front-end paging support.
+            If specified, data after the specified offset will be retrieved.
         start_after: Optional[Dict[str, Any]]
             If you pass in a serialized version of a search result
             object, the search will return results from objects after that object,
@@ -708,8 +708,8 @@ class QueryBuilder:
         Parameters
         ----------
         new_offset: Optional[int]
-            An offset for paging support in the front end.
-            If specified, data from the offset onwards will be retrieved.
+            An offset for front-end paging support.
+            If specified, data after the specified offset will be retrieved.
         """
         self.offset = new_offset
         return self
